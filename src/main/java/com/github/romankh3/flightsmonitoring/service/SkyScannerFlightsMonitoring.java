402b780766e1b206f1a441700fe61ce7b63d0506
romankh3 <roman.beskrovnyy@gmail.com>
2019-11-11 09:41:12 +0200

    init

--- src/main/java/com/github/romankh3/flightsmonitoring/service/SkyScannerFlightsMonitoring.java
+++ src/main/java/com/github/romankh3/flightsmonitoring/service/SkyScannerFlightsMonitoring.java
@@ -2,9 +2,11 @@ package com.github.romankh3.flightsmonitoring.service;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
+@Slf4j
 @Service
 public class SkyScannerFlightsMonitoring implements FlightsMonitoring {
 
@@ -16,6 +18,6 @@ public class SkyScannerFlightsMonitoring implements FlightsMonitoring {
                 .header("x-rapidapi-key", "8c78203b8bmshfb1bcfddd95d0f9p1d08dfjsnff95927bab34")
                 .asString();
 
-        System.out.println(response);
+        log.info(response.getBody());
     }
 }
