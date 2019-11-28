850433565f59b765939ed8811c3cac057278170a
romankh3 <roman.beskrovnyy@gmail.com>
2019-11-28 11:32:35 +0200

    added javadocs

--- src/main/java/com/github/romankh3/flightsmonitoring/config/WebConfig.java
+++ src/main/java/com/github/romankh3/flightsmonitoring/config/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Web configuration class.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
