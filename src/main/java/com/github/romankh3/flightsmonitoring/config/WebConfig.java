0bf4fd21e26db09cb54f60c1fd71fe93bf3f9abe
romankh3 <roman.beskrovnyy@gmail.com>
2019-11-28 11:19:38 +0200

    * added webConfig with redirect from root "/" to swagger UI page.

---
+++ src/main/java/com/github/romankh3/flightsmonitoring/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.github.romankh3.flightsmonitoring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
+    }
+}
