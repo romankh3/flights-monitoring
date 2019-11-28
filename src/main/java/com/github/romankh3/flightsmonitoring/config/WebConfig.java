4f7992f16269a324261adf44d6fb19e4a2c077b2
romankh3 <roman.beskrovnyy@gmail.com>
2019-11-28 12:24:42 +0200

    removed HomeController

---
+++ src/main/java/com/github/romankh3/flightsmonitoring/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.github.romankh3.flightsmonitoring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web configuration class.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
+    }
+}
